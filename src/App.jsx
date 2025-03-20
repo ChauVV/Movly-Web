e164fe7a89f92d80c139c0e141579dfff69d0945
DESKTOP-CV4EUQV\ChauPC <chau@devinci.io>
2025-03-20 20:48:49 +0700

    Remove basename for custom domain

--- src/App.jsx
+++ src/App.jsx
@@ -49,7 +49,7 @@ function App() {
   return (
     <WagmiProvider config={config}>
       <QueryClientProvider client={queryClient}>
-        <Router basename="/HealthStepPage">
+        <Router>
           <Routes>
             {/* Calculator tách riêng không có Header */}
             <Route path="/calculate" element={<CalculateTokenEarn />} />
